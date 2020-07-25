64dcd591744caa824f444ffdb29746ac8e841bb4
Mitch Laube <mlaube97@yahoo.com>
2020-07-25 12:21:28 -0500

    Initial Work

---
+++ Budget.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MitchBudget
+{
+    public class Budget
+    {
+        private string _name;
+        private float _amount;
+        private float _remaining;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public float Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+        public float Remaining
+        {
+            get { return _remaining; }
+            set { _remaining = value; }
+        }
+        public float Surplus
+        {
+            get { return _amount - _remaining; }
+        }
+
+        public Budget(string name, float amount, float remaining)
+        {
+            _name = name;
+            _amount = amount;
+            _remaining = remaining;
+        }
+        public void Spend(float value)
+        {
+            _remaining -= value;
+        }
+
+        public void Receive(float value)
+        {
+            _remaining += value;
+        }
+
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MitchBudget
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Budget> budgets = new List<Budget>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreateBudget_Click(object sender, RoutedEventArgs e)
+        {
+            bool filled = !String.IsNullOrEmpty(textboxAmount.Text) && !String.IsNullOrEmpty(textboxName.Text) && !String.IsNullOrEmpty(textboxRemaining.Text);
+            
+            if ( filled == true)
+            {
+                Budget budget = new Budget(textboxName.Text, (float)Convert.ToDouble(textboxAmount.Text), (float)Convert.ToDouble(textboxRemaining.Text));
+                budgets.Add(budget);
+                gridBudget.Items.Add(budget);
+            }
+        }
+    }
+}
